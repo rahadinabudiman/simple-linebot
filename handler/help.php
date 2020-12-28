04f167aaa0de573ed45eec81157509fc0c90e046
Raha <erbees.50000@gmail.com>
2020-12-28 13:01:04 +0700

    fixed bug

--- handler/help.php
+++ handler/help.php
@@ -4,7 +4,7 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
 function help($query, $userId){
 	
-	$result = new TextMessageBuilder("LINE Messenger BOT k. Commands help\n\n \n\n /aboutme : Tentang Saya \n\n /calculate : Perhitungan -- /hitung for more info \n\n /teach : Teach Word and Answer \n\n © Rahadina Budiman Sundara, 2020");
+	$result = new TextMessageBuilder("LINE Messenger BOT k. Commands help \n\n /aboutme : Tentang Saya \n\n /calculate : Perhitungan -- /hitung for more info \n\n /teach : Teach Word and Answer \n\n /xkcd : Share random photos from xkcd \n\n © Rahadina Budiman Sundara, 2020");
 	return $result;
 	
 }
