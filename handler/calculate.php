de896e06b9267cc68ff701780e36b3f73b4479ef
Raha <erbees.50000@gmail.com>
2020-12-28 15:17:04 +0700

    final commit

--- handler/calculate.php
+++ handler/calculate.php
@@ -5,7 +5,7 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 function hitung($query, $userId){
 	
 	if ($query == null){
-		$result = new TextMessageBuilder("MathJS Calculator\n\nCara Pakai:\n/Hitung (query)\n\nExample:\n/Hitung (3e+2i)*(2e-3i) atau 3+1");
+		$result = new TextMessageBuilder("MathJS Calculator\n\nCara Pakai:\n/Hitung (query)\n\nExample:\n/Hitung (3+2)*(2-3) atau 3+1");
 	} else {
 		
 		$query = urlencode($query);
