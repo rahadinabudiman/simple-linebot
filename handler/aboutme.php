c57566f329ee81c0d4fb3205b636a51f63a3d077
Raha <erbees.50000@gmail.com>
2020-12-28 12:38:54 +0700

    deploy

--- handler/aboutme.php
+++ handler/aboutme.php
@@ -4,7 +4,7 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
 function aboutme($query, $userId){
 	
-	$result = new TextMessageBuilder("LINE Messenger Bot\n\n© YETTI SKUWAD, 2018");
+	$result = new TextMessageBuilder("LINE Messenger Bot\n\n© Rahadina Budiman Sundara, 2020");
 	return $result;
 	
 }
---
+++ handler/kerang.php
@@ -0,0 +1,16 @@
+<?php
+
+
+function kerang($query, $userId){
+    $list_jwb = array(
+		'Ya',
+		'Tidak',
+		'Bisa jadi',
+		'Mungkin',
+		'Tentu tidak',
+		'Coba tanya lagi'
+		);
+    $jaws = array_rand($list_jwb);
+    $jawab = $list_jwb[$jaws];
+    return($kerang);
+}
