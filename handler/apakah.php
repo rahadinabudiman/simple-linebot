ebb92bff3777346f75325632dd8c4837584daafa
Raha <erbees.50000@gmail.com>
2020-12-28 13:36:22 +0700

    adding some fitur

--- handler/apakah.php
+++ handler/apakah.php
@@ -6,7 +6,7 @@ function kerang($query, $userId){
     if ($query == null){
         $result = new TextMessageBuilder("Kerang Ajaib Memberi Anda Sebuah Jawaban!\n\nCara Pakai:\n/kerang [apakah] [pertanyaan]\n\nExample:\n/kerang apakah aku lucu?");
     }
-        if($query == "apakah"){
+        else {
             $list_jwb = array(
                 'Ya',
                 'Tidak',
@@ -14,13 +14,10 @@ function kerang($query, $userId){
                 'Mungkin',
                 'Tentu tidak',
                 'Coba tanya lagi'
-                );
+                 );
             $jaws = array_rand($list_jwb);
             $jawab = $list_jwb[$jaws];
             $result = new TextMessageBuilder($jawab);
         }
-            else {
-                $result = new TextMessageBuilder("Kata kunci salah.");
-            }
 return($result);
 }
--- handler/help.php
+++ handler/help.php
@@ -4,7 +4,7 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
 function help($query, $userId){
 	
-	$result = new TextMessageBuilder("LINE Messenger BOT k. Commands help \n\n /aboutme : Tentang Saya \n\n /userid : Memberi tahu userid Anda \n\n /kerang : Kerang ajaib akan memberi tahu anda sebuah jawaban! \n\n /hitung : Perhitungan -- /hitung for more info \n\n /teach : Teach Word and Answer \n\n /xkcd : Share random photos from xkcd \n\n © Rahadina Budiman Sundara, 2020");
+	$result = new TextMessageBuilder("LINE Messenger BOT k. Commands help \n\n /aboutme : Tentang Saya \n\n /userid : Memberi tahu userid Anda \n\n /apakah : Kerang ajaib akan memberi tahu anda sebuah jawaban! \n\n /hitung : Perhitungan -- /hitung for more info \n\n /teach : Teach Word and Answer \n\n /xkcd : Share random photos from xkcd \n\n © Rahadina Budiman Sundara, 2020");
 	return $result;
 	
 }
