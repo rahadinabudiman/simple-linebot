854c30ffc6f57950ae9f1b3c4c4011dc4e844519
Raha <erbees.50000@gmail.com>
2020-12-28 13:29:26 +0700

    fixed bug2

--- handler/kerang.php
+++ handler/kerang.php
@@ -4,20 +4,24 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
 function kerang($query, $userId){
     if ($query == null){
-		$result = new TextMessageBuilder("Kerang Ajaib Memberi Anda Sebuah Jawaban!\n\nCara Pakai:\n/kerang [apakah] [pertanyaan]\n\nExample:\n/kerang apakah aku lucu?");
-	} else {
-    $list_jwb = array(
-		'Ya',
-		'Tidak',
-		'Bisa jadi',
-		'Mungkin',
-		'Tentu tidak',
-		'Coba tanya lagi'
-		);
-    $jaws = array_rand($list_jwb);
-    $jawab = $list_jwb[$jaws];
-    $result = new TextMessageBuilder($jawab);
-
-}
+        $result = new TextMessageBuilder("Kerang Ajaib Memberi Anda Sebuah Jawaban!\n\nCara Pakai:\n/kerang [apakah] [pertanyaan]\n\nExample:\n/kerang apakah aku lucu?");
+        if($query == "apakah"){
+            $list_jwb = array(
+                'Ya',
+                'Tidak',
+                'Bisa jadi',
+                'Mungkin',
+                'Tentu tidak',
+                'Coba tanya lagi'
+                );
+            $jaws = array_rand($list_jwb);
+            $jawab = $list_jwb[$jaws];
+            $result = new TextMessageBuilder($jawab);
+        }
+            else {
+                $result = new TextMessageBuilder("Kata kunci salah.");
+            }
+    } 
+    
 return($result);
 }
