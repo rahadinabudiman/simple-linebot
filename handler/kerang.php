89ddd323c568b1b6142f87d39cfc72dc9789ca85
Raha <erbees.50000@gmail.com>
2020-12-28 13:39:14 +0700

    adding some fitur3

--- handler/kerang.php
+++ handler/kerang.php
@@ -2,7 +2,7 @@
 
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder as TextMessageBuilder;
 
-function kerang($query, $userId){
+function apakah($query, $userId){
     if ($query == null){
         $result = new TextMessageBuilder("Kerang Ajaib Memberi Anda Sebuah Jawaban!\n\nCara Pakai:\n/kerang [apakah] [pertanyaan]\n\nExample:\n/kerang apakah aku lucu?");
     }
